6c06c6421cfaed894077ad4592a126ee63ab25b6
stefandnfr <dendorferstefan@gmail.com>
2023-06-27 20:16:45 +0200

    update pull third party script

--- pull_third_party.sh
+++ pull_third_party.sh
@@ -13,7 +13,3 @@ $MAKEFILE_DIR/flatbuffers_download.sh $DOWNLOADS_DIR $TENSORFLOW_ROOT
 $MAKEFILE_DIR/kissfft_download.sh $DOWNLOADS_DIR $TENSORFLOW_ROOT
 $MAKEFILE_DIR/pigweed_download.sh $DOWNLOADS_DIR $TENSORFLOW_ROOT
 
-
-git clone https://github.com/google/gemmlowp.git ./third_party_generated/gemmlowp
-
-git clone https://github.com/google/ruy.git ./third_party_generated/ruy
